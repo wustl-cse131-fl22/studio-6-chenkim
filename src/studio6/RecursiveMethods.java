41fd10750b6d27883eca6cda4702adb39d541f3e
MSI-NB <MSI-NB@172.27.78.76>
2022-10-26 13:48:49 -0700

    slay

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,10 +12,13 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
-		
+			if (n == 0){
+				return 0;
+			}
+			else {
+				return Math.pow(0.5, n)+ geometricSum(n-1);
+			}
+			// FIXME compute the geometric sum for the first n terms recursively		
 	}
 
 	/**
@@ -27,10 +30,12 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
-			// FIXME compute the gcd of p and q using recursion
-			return 0;
-		
+			if (p%q == 0) {
+				return q;
+			}
+			else {
+				return gcd(q, p%q);
+			}		
 	}
 
 	
@@ -41,11 +46,26 @@ public class RecursiveMethods {
 	 * @param array the array to create a reverse of, not to be mutated
 	 * @return an array with the same data as the input but it reverse order
 	 */
+	public static int[] helper(int[] array, int index) {
+			if (index == array.length/2) {
+				return array;
+			}
+			else {
+				int mirrorIndex = array.length - 1 - index;
+				int temp = array[index];
+				array[index] = array[mirrorIndex];
+				array[mirrorIndex] = temp;
+				return helper(array, ++index);
+			}
+	}
 	public static int[] toReversed(int[] array) {
-		
-			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
+			int index = 0;
+			if (index == array.length) {
+				return array;
+			}
+			else {
+				return helper(array, index);
+			}
 	}
 
 	/**
@@ -59,7 +79,12 @@ public class RecursiveMethods {
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
-		
+		if (radius == radiusMinimumDrawingThreshold) {
+			StdDraw.circle(xCenter, yCenter, radius);
+		}
+		else {
+			circlesUponCircles(xCenter/2, yCenter/2, radius/2, radiusMinimumDrawingThreshold);
+		}
 		// FIXME
 	}
 
